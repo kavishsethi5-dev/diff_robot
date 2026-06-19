8aef693c1ab27e5f39d0e16dc454c5edc66d1b17
kavishsethi5-dev <kavishsethi5@gmail.com>
2026-06-19 15:25:35 +0530

    Refactor comments in diff_PID.py for clarity
    Updated comments for clarity and consistency throughout the code.

--- scripts/diff_PID.py
+++ scripts/diff_PID.py
@@ -17,27 +17,27 @@ class diff_PID(Node):
         self.target_y=0.0
 
         # Defined once in __init__
+        #These values can be further tuned 
         self.kp = 1.2
         self.ki= 0.01
         self.kd = 0.05
         ####Initializing variables####
         self.last_error = 0.0
         self.error_sum = 0.0
-         #  Extract Current Position (X, Y)
+         # Current Position (X, Y)
         self.x_current = 0.0
         self.y_current = 0.0
 
-        # 2. Extract and Calculate Current Heading (Theta)
+        # 2. Orientation
         self.qx = 0.0
         self.qy = 0.0
         self.qz = 0.0
         self.qw = 0.0
 
 
-        #for now setting dt as 0.01, later I can see it in gazebo physics
+        #for timestep the value can be found in gazebo physics
 
-
-        #create timer?
+        #create timer
         time_period=0.01
         self.timer = self.create_timer( time_period , self.PID_callback)
 
@@ -58,6 +58,7 @@ class diff_PID(Node):
         # Math Formula 3: Angular Error
         angle_error = alpha - theta_current
         angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+        
         #Normalize angle_error to stay within [-pi, pi] eg 90 vs -270
 
 
@@ -85,7 +86,7 @@ class diff_PID(Node):
         if distance_error < 0.1:  #this was done to stop it from circling at the end
             fmsg = Twist()   # zero velocity
             self.publishers_cmd_vel_.publish(fmsg)
-            return           # exits PID_callback right here
+            return           
 
 
         fmsg = Twist()
@@ -97,14 +98,7 @@ class diff_PID(Node):
         self.publishers_cmd_vel_.publish(fmsg)
 
         
-        
-
-
-
-
-        
-
-      
+    
     def odom_callback(self, msg):# 1. Extract the raw 4D Quaternion fields
         
        #now saving as instance variables
@@ -122,16 +116,10 @@ class diff_PID(Node):
     
 
     def target_callback(self, FMAmsg):
-        #FMAmsg = Float64MultiArray() # with this we can specify the target position
-        # also PoseStamped can be used for this purpose
-        #FMAmsg.data = [5.0, 5.0, 0.0]
-        #self.subscriber_odom_.publish(FMAmsg)
-         #all this is not needed if we are getting this FMAmsg from a terminal or another node
+        # PoseStamped also can be used for this purpose
+        #we are getting this FMAmsg from a terminal or another node(in our case)
         self.target_x = FMAmsg.data[0]
         self.target_y = FMAmsg.data[1]
-
-
-
 
 
 def main(args=None):  
